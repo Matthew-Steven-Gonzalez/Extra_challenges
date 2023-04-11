1adc52d23bb8dc7961dd79718548aea3feedcb5f
Matthew Steven Gonzalez <gonzalez.matthew.steven@gmail.com>
2023-04-11 18:06:35 -0400

    begun dice Game

---
+++ Dice Game files/Dice.java
@@ -0,0 +1,14 @@
+package Dice Game files;
+
+public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    
+    int player1[] = new int[5];
+
+    int player2[] = new int[5];
+
+    confirmation = scanner.nextline();
+
+    
+}
